248002d2d6c9e27a3f79a508a6606a7ad163ee3a
gh <cfhmgh@163.com>
2023-05-29 02:55:37 +0000

    getIPWithPattern, still bugs in nested RPC...

--- include/utils/net_utils.h
+++ include/utils/net_utils.h
@@ -2,7 +2,9 @@
 #define NET_UTILS_H_
 
 #include <arpa/inet.h>
+#include <ifaddrs.h>
 #include <net/if.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <sys/ioctl.h>
@@ -23,6 +25,43 @@ inline std::string getIP(const std::string &if_name) {
     close(fd);
 
     return std::string(inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
+}
+
+inline std::string getIPWithPattern(const std::string &pattern) {
+    struct ifaddrs *ifaddr, *ifa;
+    int family, s;
+    char host[NI_MAXHOST];
+    std::string ip;
+
+    if (getifaddrs(&ifaddr) == -1) {
+        perror("getifaddrs");
+        exit(EXIT_FAILURE);
+    }
+
+    for (ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == nullptr) {
+            continue;
+        }
+
+        family = ifa->ifa_addr->sa_family;
+
+        if (family == AF_INET) {
+            s = getnameinfo(ifa->ifa_addr, sizeof(struct sockaddr_in), host, NI_MAXHOST, nullptr, 0, NI_NUMERICHOST);
+
+            if (s != 0) {
+                printf("getnameinfo() failed: %s\n", gai_strerror(s));
+                exit(EXIT_FAILURE);
+            }
+
+            if (std::string(host).find(pattern) != std::string::npos) {
+                ip = std::string(host);
+                break;
+            }
+        }
+    }
+
+    freeifaddrs(ifaddr);
+    return ip;
 }
 
 inline std::string getMac(const std::string &if_name) {
